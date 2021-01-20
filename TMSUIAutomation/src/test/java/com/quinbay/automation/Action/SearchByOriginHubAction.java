7a031aa2a6019a3826bdc404adf7797f791a8b42
Indhu K <indhuk@Indhus-MacBook-Pro.local>
2021-01-21 01:16:34 +0530

    firstcommit

--- TMSUIAutomation/src/test/java/com/quinbay/automation/Action/SearchByOriginHubAction.java
+++ TMSUIAutomation/src/test/java/com/quinbay/automation/Action/SearchByOriginHubAction.java
@@ -1,2 +1,21 @@
-package com.quinbay.automation.Action;public class SearchByOriginHubAction {
+package com.quinbay.automation.Action;
+
+import com.quinbay.automation.Pages.SearchByOriginHubPage;
+import com.quinbay.automation.Pages.SortByOriginHubPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class SearchByOriginHubAction {
+    SearchByOriginHubPage searchConnectionPage;
+    public SearchByOriginHubAction(WebDriver driver) {
+        searchConnectionPage = PageFactory.initElements(driver, SearchByOriginHubPage.class);
+    }
+
+    public void ClickOnOriginHub() {
+        searchConnectionPage.ClickOnOriginHubToSearch();
+    }
+
+    public void chooseHubForSearch() {
+        searchConnectionPage.chooseHubThatNeedsToBeSearched();
+    }
 }
