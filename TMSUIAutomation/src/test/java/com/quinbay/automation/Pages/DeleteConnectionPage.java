d0e5a4045570bbd347429e979bdd73443f52ec0b
Indhu K <indhuk@Indhus-MacBook-Pro.local>
2021-01-21 01:22:54 +0530

    firstcommit

--- TMSUIAutomation/src/test/java/com/quinbay/automation/Pages/DeleteConnectionPage.java
+++ TMSUIAutomation/src/test/java/com/quinbay/automation/Pages/DeleteConnectionPage.java
@@ -1,2 +1,19 @@
-package com.quinbay.automation.Pages;public class DeleteConnectionPage {
+package com.quinbay.automation.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class DeleteConnectionPage {
+
+    WebDriver driver;
+    public DeleteConnectionPage(WebDriver webDriver){
+        driver=webDriver;
+    }
+
+    @FindBy(xpath = "(//div//div//table//span//img[@class='icon margin-l10'])[2]")
+    WebElement delete;
+    public void clickDelete() {
+        delete.click();
+    }
 }
