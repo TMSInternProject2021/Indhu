226c6db5891b35c57e688c7345a303e3dbfb1b9a
Indhu K <indhuk@Indhus-MacBook-Pro.local>
2021-01-21 01:28:06 +0530

    firstcommit

--- TMSUIAutomation/src/test/java/com/quinbay/automation/steps/EditConnectionSteps.java
+++ TMSUIAutomation/src/test/java/com/quinbay/automation/steps/EditConnectionSteps.java
@@ -1,2 +1,36 @@
-package com.quinbay.automation.steps;public class EditConnectionSteps {
+package com.quinbay.automation.steps;
+
+import com.quinbay.automation.Action.AddNewConnectionAction;
+import com.quinbay.automation.Action.EditConnectionAction;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+
+public class EditConnectionSteps {
+    EditConnectionAction editAction;
+    WebDriver driver;
+
+    @Given("User should be on linehauldetails page")
+    public void lineHaulPage(){
+        driver = LoginStep.driver;
+        editAction=new EditConnectionAction(driver);
+        System.out.println("Edit Connection");
+    }
+
+    @When("user clicks edit icon for particular connection")
+    public void clickOnEdit() {
+        editAction.editConnection();
+    }
+
+    @And("user edit the fields and clicks on save")
+    public void editFieldsClickOnSave() throws InterruptedException {
+        editAction.editFieldsAndSave();
+    }
+
+    @Then("validate if the connection has been edited")
+    public void editSuccess() {
+        System.out.println("Connection edited successfully");
+    }
 }
