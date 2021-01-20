8237843704ba5341c37c4a3fb8e6c3309dd67ef0
Indhu K <indhuk@Indhus-MacBook-Pro.local>
2021-01-21 01:23:35 +0530

    firstcommit

--- TMSUIAutomation/src/test/java/com/quinbay/automation/Pages/EditConnectionPage.java
+++ TMSUIAutomation/src/test/java/com/quinbay/automation/Pages/EditConnectionPage.java
@@ -1,2 +1,43 @@
-package com.quinbay.automation.Pages;public class EditConnectionPage {
+package com.quinbay.automation.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class EditConnectionPage {
+    WebDriver driver;
+
+    public EditConnectionPage(WebDriver webDriver){
+        driver=webDriver;
+    }
+
+    @FindBy(xpath="(//div//div//span//img[@class='icon'])[2]")
+    WebElement edit;
+    public void clickOnEditAction() {
+        edit.click();
+    }
+
+    @FindBy(xpath = "//div//div//div//input[@class='input-box-style dropdown']")
+    WebElement repeat;
+
+    @FindBy(xpath = "//div//div//span[contains(text(),'Monday')]")
+    WebElement editRepeat1;
+
+    @FindBy(xpath = "//div//div//span[contains(text(),'Thursday')]")
+    WebElement editRepeat2;
+
+    @FindBy(xpath = "//div//div//button[contains(text(),'Save')]")
+    WebElement save;
+
+    public void editFields() throws InterruptedException {
+        repeat.click();
+        editRepeat1.click();
+        editRepeat2.click();
+        save.click();
+        Thread.sleep(1000);
+    }
+
+
+
+
 }
