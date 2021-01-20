cf69ea03a7f58eff3321f7bcb33a59e21565949f
Indhu K <indhuk@Indhus-MacBook-Pro.local>
2021-01-21 01:19:29 +0530

    firstcommit

--- TMSUIAutomation/src/test/java/com/quinbay/automation/Action/DeleteConnectionAction.java
+++ TMSUIAutomation/src/test/java/com/quinbay/automation/Action/DeleteConnectionAction.java
@@ -1,2 +1,17 @@
-package com.quinbay.automation.Action;public class DeleteConnectionAction {
+package com.quinbay.automation.Action;
+
+import com.quinbay.automation.Pages.DeleteConnectionPage;
+import com.quinbay.automation.Pages.EditConnectionPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class DeleteConnectionAction {
+    DeleteConnectionPage deleteConnectionPage;
+    public DeleteConnectionAction(WebDriver driver) {
+        deleteConnectionPage = PageFactory.initElements(driver, DeleteConnectionPage.class);
+    }
+
+    public void ClickOnDelete() {
+        deleteConnectionPage.clickDelete();
+    }
 }
