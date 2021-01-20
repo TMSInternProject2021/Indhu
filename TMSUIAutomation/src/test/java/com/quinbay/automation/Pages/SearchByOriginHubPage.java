4d032f0eea24ec0754da980f32b9de193305fe41
Indhu K <indhuk@Indhus-MacBook-Pro.local>
2021-01-21 01:24:23 +0530

    firstcommit

--- TMSUIAutomation/src/test/java/com/quinbay/automation/Pages/SearchByOriginHubPage.java
+++ TMSUIAutomation/src/test/java/com/quinbay/automation/Pages/SearchByOriginHubPage.java
@@ -1,2 +1,26 @@
-package com.quinbay.automation.Pages;public class SearchByOriginHubPage {
+package com.quinbay.automation.Pages;
+
+import io.cucumber.java.en.Given;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SearchByOriginHubPage {
+
+    WebDriver driver;
+    public SearchByOriginHubPage(WebDriver webDriver){
+        driver=webDriver;
+    }
+    @FindBy(xpath = "//div//div//table//span//label[@class='clickable']")
+    WebElement search;
+    public void ClickOnOriginHubToSearch() {
+        search.click();
+    }
+
+    @FindBy(xpath = "(//div//div//label[contains(text(),'TMS Hub')])")
+    WebElement chooseHub;
+    public void chooseHubThatNeedsToBeSearched() {
+        chooseHub.click();
+    }
+
 }
