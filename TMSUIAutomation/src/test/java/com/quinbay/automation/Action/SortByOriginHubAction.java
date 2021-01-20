f26c1f860c6dc54235d01a7acd295f505e38e8c3
Indhu K <indhuk@Indhus-MacBook-Pro.local>
2021-01-21 01:17:13 +0530

    firstcommit

--- TMSUIAutomation/src/test/java/com/quinbay/automation/Action/SortByOriginHubAction.java
+++ TMSUIAutomation/src/test/java/com/quinbay/automation/Action/SortByOriginHubAction.java
@@ -1,2 +1,17 @@
-package com.quinbay.automation.Action;public class SortByOriginHubAction {
+package com.quinbay.automation.Action;
+
+import com.quinbay.automation.Pages.SortByOriginHubPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class SortByOriginHubAction {
+
+    SortByOriginHubPage sortConnectionPage;
+    public SortByOriginHubAction(WebDriver driver) {
+        sortConnectionPage = PageFactory.initElements(driver, SortByOriginHubPage.class);
+    }
+
+    public void clickOnDownArrowToSort() {
+        sortConnectionPage.ClickDownArrowToSort();
+    }
 }
