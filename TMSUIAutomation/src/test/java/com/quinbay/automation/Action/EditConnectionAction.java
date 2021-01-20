c0626a2b50230333530a46ba46763249515f7c66
Indhu K <indhuk@Indhus-MacBook-Pro.local>
2021-01-21 01:18:37 +0530

    firstcommit

--- TMSUIAutomation/src/test/java/com/quinbay/automation/Action/EditConnectionAction.java
+++ TMSUIAutomation/src/test/java/com/quinbay/automation/Action/EditConnectionAction.java
@@ -1,2 +1,21 @@
-package com.quinbay.automation.Action;public class EditConnectionAction {
+package com.quinbay.automation.Action;
+
+import com.quinbay.automation.Pages.AddNewConnectionPage;
+import com.quinbay.automation.Pages.EditConnectionPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class EditConnectionAction {
+    EditConnectionPage editConnectionPage;
+    public EditConnectionAction(WebDriver driver) {
+        editConnectionPage = PageFactory.initElements(driver, EditConnectionPage.class);
+    }
+
+    public void editConnection() {
+        editConnectionPage.clickOnEditAction();
+    }
+
+    public void editFieldsAndSave() throws InterruptedException {
+        editConnectionPage.editFields();
+    }
 }
