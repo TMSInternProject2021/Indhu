11e43f551a73bbd71124459819b393a73b7d2fe4
Indhu K <indhuk@Indhus-MacBook-Pro.local>
2021-01-21 01:21:16 +0530

    firstcommit

--- TMSUIAutomation/src/test/java/com/quinbay/automation/Pages/LoginPages.java
+++ TMSUIAutomation/src/test/java/com/quinbay/automation/Pages/LoginPages.java
@@ -21,28 +21,30 @@ public class LoginPages {
     @FindBy(id = "password")
     WebElement pass;
 
-    @FindBy(xpath="//*[@id=\"loginCard\"]/form/div[4]/input")
+    @FindBy(xpath="//div//div//input[@class='btn btn-primary submitButton']")
     WebElement loginButton;
 
     public void loginToTMS() throws InterruptedException {
         username.sendKeys("tmsadmin");
         pass.sendKeys("Test@2020");
        // driver.manage().timeouts().implicitlyWait(1000, SECONDS);
-        Thread.sleep(100);
+        //Thread.sleep(1000);
         loginButton.click();
+        Thread.sleep(1000);
         //driver.manage().timeouts().implicitlyWait(1000, SECONDS);
 
     }
 
-    @FindBy(xpath="//*[@id=\"mount\"]/div/div/div[2]/div/div[2]/div[1]/div/div[3]/button")
+    @FindBy(xpath="//div//div//button[@class='default-button card_button']")
     WebElement enterToTMS;
-    public void clickOnEnterToTMS() {
+    public void clickOnEnterToTMS() throws InterruptedException {
         enterToTMS.click();
-        driver.manage().timeouts().implicitlyWait(1000, SECONDS);
+      //  driver.manage().timeouts().implicitlyWait(1000, SECONDS);
+        Thread.sleep(100);
 
     }
 
-    @FindBy(xpath="//*[@id=\"mount\"]/div/div/div[2]/div/div/div/div[2]/div/div/span")
+    @FindBy(xpath="//*[@id=\"mount\"]/div/div/div[2]/div/div/div/div[2]/div/div/span/span")
     WebElement clickArrow;
 
     @FindBy(xpath = "//*[contains(@class, 'Select-input')]/..")
@@ -70,7 +72,7 @@ public class LoginPages {
 
     public void clickSettings() {
         settings.click();
-        driver.manage().timeouts().implicitlyWait(1000, SECONDS);
+      //  driver.manage().timeouts().implicitlyWait(1000, SECONDS);
         linehaul.click();
     }
 
