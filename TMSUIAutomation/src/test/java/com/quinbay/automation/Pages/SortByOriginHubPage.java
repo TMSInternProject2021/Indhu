25a5e23139fe52cf31408badb1a575e5a16b9423
Indhu K <indhuk@Indhus-MacBook-Pro.local>
2021-01-21 01:24:50 +0530

    firstcommit

--- TMSUIAutomation/src/test/java/com/quinbay/automation/Pages/SortByOriginHubPage.java
+++ TMSUIAutomation/src/test/java/com/quinbay/automation/Pages/SortByOriginHubPage.java
@@ -1,2 +1,19 @@
-package com.quinbay.automation.Pages;public class SortByOriginHubPage {
+package com.quinbay.automation.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SortByOriginHubPage {
+    WebDriver driver;
+    public SortByOriginHubPage(WebDriver webDriver){
+        driver=webDriver;
+    }
+
+    @FindBy(xpath = "//div//div//table//span[@class='icon-down-arrow down']")
+    WebElement sort;
+
+    public void ClickDownArrowToSort() {
+        sort.click();
+    }
 }
