f187d7cbebbdbb49508e0919f2408ab07779f69a
Indhu K <indhuk@Indhus-MacBook-Pro.local>
2021-01-21 15:26:56 +0530

    firstcommit

--- TMSUIAutomation/src/test/java/com/quinbay/automation/Action/MandatoryErrorMessageAction.java
+++ TMSUIAutomation/src/test/java/com/quinbay/automation/Action/MandatoryErrorMessageAction.java
@@ -1,2 +1,21 @@
-package com.quinbay.automation.Action;public class MandatoryErrorMessageAction {
+package com.quinbay.automation.Action;
+
+import com.quinbay.automation.Pages.MandatoryErrorMessagePages;
+import com.quinbay.automation.Pages.VerifyCountPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class MandatoryErrorMessageAction {
+    public MandatoryErrorMessagePages errorMessagePages;
+    public MandatoryErrorMessageAction(WebDriver driver) {
+        errorMessagePages = PageFactory.initElements(driver, MandatoryErrorMessagePages.class);
+    }
+
+    public void clickAdd() {
+        errorMessagePages.ClickOnAdd();
+    }
+
+    public void enterInvalidData() throws InterruptedException {
+        errorMessagePages.InvalidData();
+    }
 }
