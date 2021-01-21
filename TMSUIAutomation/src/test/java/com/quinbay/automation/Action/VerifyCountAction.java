ff6f11767e97bf1ed818223bebe3745abd28ed30
Indhu K <indhuk@Indhus-MacBook-Pro.local>
2021-01-21 15:26:00 +0530

    firstcommit

--- TMSUIAutomation/src/test/java/com/quinbay/automation/Action/VerifyCountAction.java
+++ TMSUIAutomation/src/test/java/com/quinbay/automation/Action/VerifyCountAction.java
@@ -1,2 +1,29 @@
-package com.quinbay.automation.Action;public class VerifyCountAction {
+package com.quinbay.automation.Action;
+
+import com.quinbay.automation.Pages.LoginPages;
+import com.quinbay.automation.Pages.VerifyCountPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class VerifyCountAction {
+    public VerifyCountPage verifyCountPage;
+    public VerifyCountAction(WebDriver driver) {
+        verifyCountPage = PageFactory.initElements(driver, VerifyCountPage.class);
+    }
+
+    public void performSearch() throws InterruptedException {
+        verifyCountPage.performSearch();
+    }
+
+    public void countNoOfEntries() {
+        verifyCountPage.countConnectionList();
+    }
+
+    public void countDisplayedAtPageEnd() {
+        verifyCountPage.CountDisplayedAtEnd();
+    }
+
+    public void validateBothCountAreEqual() {
+        verifyCountPage.check();
+    }
 }
