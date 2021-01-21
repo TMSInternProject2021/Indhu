d1f627cea87616ab6522487268fcaf59ca6f8542
Indhu K <indhuk@Indhus-MacBook-Pro.local>
2021-01-21 15:17:18 +0530

    firstcommit

--- TMSUIAutomation/src/test/java/com/quinbay/automation/steps/MandatoryErrorMessageSteps.java
+++ TMSUIAutomation/src/test/java/com/quinbay/automation/steps/MandatoryErrorMessageSteps.java
@@ -1,2 +1,34 @@
-package com.quinbay.automation.steps;public class MandatoryErrorMessageSteps {
+package com.quinbay.automation.steps;
+
+import com.quinbay.automation.Action.MandatoryErrorMessageAction;
+import com.quinbay.automation.Action.VerifyCountAction;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+public class MandatoryErrorMessageSteps {
+    MandatoryErrorMessageAction errorMessageAction;
+    WebDriver driver;
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+
+    @Given("User should be on Add new connection pop-up")
+    public void addNewConnectionPopUpPage() {
+        driver = LoginStep.driver;
+        errorMessageAction=new MandatoryErrorMessageAction(driver);
+        errorMessageAction.clickAdd();
+        System.out.println("Add new connection...");
+    }
+
+    @When("user enters invalid data and clicks on save")
+    public void enterInvalidData() throws InterruptedException {
+        errorMessageAction.enterInvalidData();
+    }
+
+    @Then("mandatory error message is thrown")
+    public void errorMessageThrown() {
+        System.out.println("Mandatory Error Message Thrown");
+    }
+
 }
