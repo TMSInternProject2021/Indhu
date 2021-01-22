388ec4b3a71164e067a509721e7ca1e792b113a0
Indhu K <indhuk@Indhus-MacBook-Pro.local>
2021-01-22 09:04:13 +0530

    first commit

--- TMSUIAutomation/src/test/java/com/quinbay/automation/steps/AddNewConnectionSteps.java
+++ TMSUIAutomation/src/test/java/com/quinbay/automation/steps/AddNewConnectionSteps.java
@@ -1,28 +1,23 @@
 package com.quinbay.automation.steps;
 
 import com.quinbay.automation.Action.AddNewConnectionAction;
-import com.quinbay.automation.Action.LoginActions;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.java.en_scouse.An;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AddNewConnectionSteps {
     AddNewConnectionAction addAction;
     WebDriver driver;
-    JavascriptExecutor js = (JavascriptExecutor) driver;
 
-    @Given("User should be on linehaul details page")
+    @Given("user should be on linehaul details page")
     public void linehaulDetailsPage() {
         driver = LoginStep.driver;
         addAction=new AddNewConnectionAction(driver);
-        System.out.println("Linehaul details page...");
     }
-
-
 
     @When("user clicks add new connection")
     public void clickOnAddNewConnection() {
@@ -30,13 +25,14 @@ public class AddNewConnectionSteps {
     }
 
     @And("user fills all details to create an new connection and clicks on save")
-    public void FilltheDetails() throws InterruptedException {
-        addAction.FillDetails();
+    public void fillTheDetails() throws InterruptedException {
+        addAction.fillDetails();
     }
 
     @Then("validate if new connection is created")
-    public void ConnectionCreated() {
-        System.out.println("ConnectionCreated...");
+    public void connectionCreated() {
+        addAction.verifyConnectionIsSuccessful();
+       // System.out.println("ConnectionCreated...");
     }
 
 }
