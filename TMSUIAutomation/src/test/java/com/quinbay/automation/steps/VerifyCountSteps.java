cc6f870b73b830f0a686ebe49579c1e547408b12
Indhu K <indhuk@Indhus-MacBook-Pro.local>
2021-01-22 11:29:47 +0530

    first commit

--- TMSUIAutomation/src/test/java/com/quinbay/automation/steps/VerifyCountSteps.java
+++ TMSUIAutomation/src/test/java/com/quinbay/automation/steps/VerifyCountSteps.java
@@ -1,30 +1,20 @@
 package com.quinbay.automation.steps;
 
-import com.quinbay.automation.Action.AddNewConnectionAction;
-import com.quinbay.automation.Action.LoginActions;
 import com.quinbay.automation.Action.VerifyCountAction;
-import io.cucumber.java.bs.A;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.java.en_scouse.An;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class VerifyCountSteps {
     VerifyCountAction verifyCountAction;
     WebDriver driver;
-    JavascriptExecutor js = (JavascriptExecutor) driver;
 
     @Given("user should be on linehaul detail page")
     public void linehaulDetailsPage() {
         driver = LoginStep.driver;
         verifyCountAction=new VerifyCountAction(driver);
-        System.out.println("Linehaul details page...");
     }
 
     @And("search operation is performed")
@@ -46,6 +36,8 @@ public class VerifyCountSteps {
     public void validateDataAndTotalCountAreEqual() {
         verifyCountAction.validateBothCountAreEqual();
     }
+
+
 
 
 }
