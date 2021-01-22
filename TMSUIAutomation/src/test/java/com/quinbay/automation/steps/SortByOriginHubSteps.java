bc1a4516013013d7fc5dc1ff76a20f39c0fc3be7
Indhu K <indhuk@Indhus-MacBook-Pro.local>
2021-01-22 11:29:30 +0530

    first commit

--- TMSUIAutomation/src/test/java/com/quinbay/automation/steps/SortByOriginHubSteps.java
+++ TMSUIAutomation/src/test/java/com/quinbay/automation/steps/SortByOriginHubSteps.java
@@ -1,11 +1,12 @@
 package com.quinbay.automation.steps;
 
-import com.quinbay.automation.Action.DeleteConnectionAction;
 import com.quinbay.automation.Action.SortByOriginHubAction;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SortByOriginHubSteps {
     SortByOriginHubAction sortAction;
@@ -15,16 +16,15 @@ public class SortByOriginHubSteps {
     public void lineHaulPage(){
         driver = LoginStep.driver;
         sortAction=new SortByOriginHubAction(driver);
-        System.out.println("Sort Connection");
     }
 
     @When("user click on origin hub downarrow")
-    public void clickOnDownArrow() {
+    public void clickOnDownArrow() throws InterruptedException {
         sortAction.clickOnDownArrowToSort();
     }
 
     @Then("user should be able to see the connection list sorted in descending order")
-    public void SortResults() {
-        System.out.println("connection list is sorted in descending order");
+    public void sortResults() {
+        assertThat("Connection is not sorted",sortAction.verifySort());
     }
 }
