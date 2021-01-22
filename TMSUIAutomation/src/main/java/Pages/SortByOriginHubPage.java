b3d097f011c0cb78ba58c528996dddaad5863ccf
Indhu K <indhuk@Indhus-MacBook-Pro.local>
2021-01-22 11:22:07 +0530

    first commit

--- TMSUIAutomation/src/main/java/Pages/SortByOriginHubPage.java
+++ TMSUIAutomation/src/main/java/Pages/SortByOriginHubPage.java
@@ -1,5 +1,6 @@
 package Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -13,7 +14,25 @@ public class SortByOriginHubPage {
     @FindBy(xpath = "//div//div//table//span[@class='icon-down-arrow down']")
     WebElement sort;
 
-    public void ClickDownArrowToSort() {
+    public void clickDownArrowToSort() throws InterruptedException {
+        Thread.sleep(1000);
         sort.click();
     }
+
+    public boolean verifySortByOriginHubInDescendingOrderIsPerformed(){
+        WebElement count=driver.findElement(By.xpath("(//div//span[@class='bold-font'])[3]"));
+        int countRows=Integer.parseInt(count.getText());
+        countRows+=1;
+        int i=1;
+        while(i+1!=countRows){
+            WebElement rowOne=driver.findElement(By.xpath("//div//table//tr["+i+"]//td"));
+            WebElement rowTwo=driver.findElement(By.xpath("//div//table//tr["+(i+1)+"]//td"));
+            if(rowTwo!=null && rowOne.getText().compareTo(rowTwo.getText())<0){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+
 }
